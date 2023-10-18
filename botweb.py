6ff6395ed375a028a5c3ca73aa629e2439338e53
Saathvik-Sheerla <140387813+Saathvik-Sheerla@users.noreply.github.com>
2023-10-18 17:00:57 +0530

    botweb.py

---
+++ botweb.py
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Telegram Bot Services</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Telegram Bot Services</h1>
+        <p>Explore the capabilities of our Telegram bot!</p>
+    </header>
+
+    <section class="feature">
+        <h2>Feature 1: Message Forwarding</h2>
+        <p>Our bot can forward messages from one channel to another efficiently.</p>
+    </section>
+
+    <section class="feature">
+        <h2>Feature 2: Custom Commands</h2>
+        <p>Create custom commands to automate tasks and provide information to users.</p>
+    </section>
+
+    <section class="feature">
+        <h2>Feature 3: Real-time Updates</h2>
+        <p>Get real-time updates and notifications from various sources using our bot.</p>
+    </section>
+
+    <footer>
+        <p>Contact us at: <a href="mailto:forwardbot28@gmail.com">forwardbot28@gmail.com</a></p>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+
+</html><footer>
+    
+<form action="https://formsubmit.co/thirumalesh.sagar1@gmail.com" method="POST">
+<input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your em@il" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+</form>
+
+</footer>
+
+</html>
